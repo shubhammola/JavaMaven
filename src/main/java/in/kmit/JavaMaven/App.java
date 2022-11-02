6b3be7c30ed796b15116b8f9f4c78cc3bfda8cce
Shubham <shubhammola@Shubhams-MacBook-Air.local>
2022-11-02 11:09:38 +0530

    New Project

---
+++ src/main/java/in/kmit/JavaMaven/App.java
@@ -0,0 +1,13 @@
+package in.kmit.JavaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey, Shubham here!" );
+    }
+}
---
+++ src/test/java/in/kmit/JavaMaven/AppTest.java
@@ -0,0 +1,20 @@
+package in.kmit.JavaMaven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
